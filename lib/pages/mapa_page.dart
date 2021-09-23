0222b4e6f6f4098ad34920b621dd61fa6cba19de
paco <paco.33353@gmail.com>
2021-09-22 20:10:41 -0500

    ya mestra el mapa y funciona correctamente

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mapa_app/blocs/ubication/ubication_bloc.dart';
 
 
@@ -27,15 +28,33 @@ class _MapaPageState extends State<MapaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocBuilder<UbicationBloc, UbicationState>(
-        builder: (context, state) {
-          return Center(
-            child: Text(
-              '${state.ubicacion?.latitude}, ${state.ubicacion?.longitude}',
-              style: TextStyle( fontSize: 20 ),
-            ),
-          );
-        },
+        builder: (context, state)=> coordenadas(state)
       )
     );
   }
+}
+
+coordenadas( UbicationState state ){
+  if ( state.ubicacion == null ) return Center(
+    child: Text('Calculando'),
+  );
+
+  // else return Center(
+  //   child: Text(
+  //     '${state.ubicacion?.latitude}, ${state.ubicacion?.longitude}',
+  //     style: TextStyle( fontSize: 20 ),
+  //   ),
+  // );
+
+  final _camaraPosition = new CameraPosition(
+    target: state.ubicacion!,
+    zoom: 15
+  );
+
+  return GoogleMap(
+    initialCameraPosition: _camaraPosition,
+    compassEnabled: true,
+    myLocationEnabled: true,
+    myLocationButtonEnabled: true,
+  );
 }
